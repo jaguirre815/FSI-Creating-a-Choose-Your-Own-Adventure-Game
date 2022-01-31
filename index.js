42197dca0f6c8ddf35c9839b539bdf417d350f2d
Joel Aguirre <Joels@Joshuas-MacBook-Pro.local>
2022-01-31 03:28:14 -0600

    i tried to fix it but failed

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 let firstAnswer = window.prompt('Do you head left or right?')
 if (firstAnswer === 'left'){
     let secondAnswer = window.prompt('you come across a stray cat.It scampers off down a small hole, just larger enough for you to crawl through.Do you follow it, or continue on your path?')
-    if (secondAnswer ==='follow'){
+    if (secondAnswer ==='follow it'){
         let thirdanswer = window.prompt('You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe heaven.')
         if (thirdanswer ==='stay'){
             window.alert('you live happily amongst the cats for the rest of you days.')
         }
         else if(thirdAnswer === 'spread the word'){
-            window.prompt('After leavin the cat colony, you are never able to find it again, without proof, no one believes your story which passes into legend nonetheless')
+           let thirdAnswer = window.alert('After leavin the cat colony, you are never able to find it again, without proof, no one believes your story which passes into legend nonetheless')
     
     if(secondAnswer === 'continue'){
         let thirdAnswer = window.prompt('you come across chamber that reaches upward to a shining light above. There is a ling, winding staircase. and a much quicker, but rickety-looking ladder that leads up toward the light. Which do you take?')
         if(thirdAnswer === 'ladder'){
-            window.alert('After ascending a few feet up the ladder, one of its rungs snaps, and you comedically fall through each of the rungs below. Sheepish, you return home.')
+           let fourthAnswer = window.alert('After ascending a few feet up the ladder, one of its rungs snaps, and you comedically fall through each of the rungs below. Sheepish, you return home.')
         }
         else if(thirdAnswer === "staircase"){
-            window.alert('After ascending the staircase, you discover a shinny blue stone, which you take home and cherish forever.')
+            let fourthAnswer = window.alert('After ascending the staircase, you discover a shinny blue stone, which you take home and cherish forever.')
         } 
     }
 }           
